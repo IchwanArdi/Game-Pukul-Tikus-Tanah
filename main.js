1b0a59c832f4fb59666c466b71d8f48a2e3404ee
IchwanArdi <ichwanpwt22@gmail.com>
2025-03-23 08:45:08 +0700

    first commit

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const btnPlay = document.querySelector('.btnPlay');
 
 let gameBerjalan = false;
 let waktu = 30;
-const soundtrack = new Audio('./asset/soundtrack.mp3');
+const soundtrack = new Audio('./asset/music/soundtrack.mp3');
 soundtrack.loop = true; // Musik loop selama main
 
 // Fungsi untuk memunculkan tikus secara acak
@@ -31,7 +31,7 @@ function score() {
       if (!e.target.classList.contains('kena')) {
         number.innerHTML++;
         e.target.classList.add('kena'); // Mencegah klik ganda
-        new Audio('./asset/hitt.mp3').play(); // Suara hit
+        new Audio('./asset/music/hitt.mp3').play(); // Suara hit
         setTimeout(() => e.target.classList.remove('kena'), 500); // Reset klik setelah 0.5 detik
       }
     });
@@ -51,7 +51,7 @@ function play() {
       alertBox.style.display = 'block';
       scoreTotal.innerHTML = number.innerHTML;
       soundtrack.pause(); // Stop musik
-      new Audio('./asset/negative_beeps-6008.mp3').play(); // Suara game over
+      new Audio('./asset/music/negative_beeps-6008.mp3').play(); // Suara game over
     }
   }, 1000);
 }
